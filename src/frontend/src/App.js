2a2b471e75c00e440a21f84ddae76571651d9d38
Kushal Gupta <kushal.gupta@hashedin.com>
2021-04-15 13:24:03 +0530

    Completed UI components

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import {TeamPage} from "./Pages/TeamPage";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {MatchPage} from "./Pages/MatchPage";
 
 function App() {
   return (
     <div className="App">
-        <TeamPage />
+        <Router>
+            <Switch>
+                <Route path="/teams/:teamName/matches/:year">
+                    <MatchPage />
+                </Route>
+                <Route path="/teams/:teamName">
+                    <TeamPage />
+                </Route>
+            </Switch>
+        </Router>
     </div>
   );
 }
---
+++ src/frontend/src/Pages/MatchPage.js
@@ -0,0 +1,17 @@
+import React, {useEffect, useState} from "react";
+import {MatchDetailsCard} from "../Components/MatchDetailsCard";
+import {MatchSmallCard} from "../Components/MacthSmallCard";
+import {useParams} from "react-router-dom";
+
+export const MatchPage = () => {
+
+
+
+    return (
+        <div className="TeamPage">
+            <h1>MatchPage</h1>
+
+        </div>
+    );
+}
+
